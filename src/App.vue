6b65a0b8819fdf5c3da7b6a98cf7fe435c65d01c
tarinus-htd <victorienhountondji23@gmail.com>
2023-08-25 13:40:23 +0100

    VueCounterApp

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+<template>
+  <div class="bg-slate-200 w-screen h-screen">
+    <div class="flex items-center justify-center h-full">
+      <div class="text-center">
+        <h4 class="text-2xl">
+          The current count is <span>{{ count }}</span>
+        </h4>
+        <h1 class="text-7xl">{{ count }}</h1>
+        <div class="mt-4">
+          <button class="border py-1 px-8 rounded bg-emerald-300" @click="count--">-</button>
+          <button class="border py-1 px-8 rounded bg-emerald-300" @click="count++">+</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
